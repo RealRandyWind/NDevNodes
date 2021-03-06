69084ed5f08a4899d0c2db7805bbd87e11f790cf
Randy Wind <wind.randy@gmail.com>
2021-03-06 17:29:31 +0100

    Initialize Visual Studio development base, extend boiler base

---
+++ NDevNodesTEST/NDevNodesTEST.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NDevNodesTEST
+{
+	TEST_CLASS(NDevNodesTEST)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
